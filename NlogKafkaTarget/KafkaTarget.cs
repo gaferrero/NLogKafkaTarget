0aabcd5655b6c6ffdcbf788cca022934dc3dc176
Gustavo <gustavoadolfo.ferrero@gmail.com>
2018-09-21 14:47:12 -0300

    This library helps to save log's messages on the Kafca queue.

---
+++ NlogKafkaTarget/KafkaTarget.cs
@@ -0,0 +1,51 @@
+﻿using KafkaNet;
+using KafkaNet.Model;
+using KafkaNet.Protocol;
+using NLog;
+using NLog.Config;
+using NLog.Targets;
+using System;
+using System.Collections.Generic;
+
+namespace NlogKafkaTarget
+{
+	[Target("KafkaWrapper")]
+	public sealed class KafkaTarget : TargetWithLayout
+	{
+		public KafkaTarget()
+		{
+			this.Host = "localhost";
+			this.Topic = "Test";
+		}
+
+		[RequiredParameter]
+		public string Host { get; set; }
+
+		[RequiredParameter]
+		public string Topic { get; set; }
+
+
+		protected override void Write(LogEventInfo logEvent)
+		{			
+			string logMessage = this.Layout.Render(logEvent);
+
+			SendTheMessageToRemoteHost(logMessage);
+		}
+		
+
+		private void SendTheMessageToRemoteHost(string message)
+		{
+			 
+			var uri = new Uri(Host);
+
+
+			var options = new KafkaOptions(uri);
+			var msg = new Message(message);
+
+			var router = new BrokerRouter(options);
+			var client = new Producer(router);
+			client.SendMessageAsync(Topic, new List<Message> { msg }).Wait();
+
+		}
+	}
+}
